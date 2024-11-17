badcc28945354d55820baa0efce0f8d4784900c6
saivajr <alexsaivanidis@hotmail.com>
2024-11-17 20:16:56 +0200

    Change datetime to map to a date value in the database

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿namespace SMDb.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SMDb.Models
 {
     // Model view of the Movie entries in the database
     public class Movie
@@ -6,9 +8,12 @@
         public int MovieId { get; set; }
         public required string Title { get; set; }
         public required string Description { get; set; }
+
+        [Column(TypeName = "date")]
         public DateTime ReleaseDate { get; set; }
         public string? ImageUrl { get; set; }
         public string? TrailerUrl { get; set; }
+
 
     }
 }
